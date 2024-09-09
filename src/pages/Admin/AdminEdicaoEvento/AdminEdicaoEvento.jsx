8fdb90411563ed043eaeb9de099ce1c58a71fc51
Artur Dantas <arturdantasrodrigues@gmail.com>
2024-09-08 22:15:17 -0300

    [EV-17] - feat: adiciona componente de editor de texto | cria página de edição de evento

--- src/pages/Admin/AdminEdicaoEvento/AdminEdicaoEvento.jsx
+++ src/pages/Admin/AdminEdicaoEvento/AdminEdicaoEvento.jsx
@@ -1,69 +1,146 @@
 import { useParams } from 'react-router-dom';
-
-// src/Tiptap.tsx
-import {
-	useEditor,
-	EditorContent,
-	FloatingMenu,
-	BubbleMenu,
-} from '@tiptap/react';
+import TextEditor from '../../../components/AdminModule/TextEditor/TextEditor';
+import { useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { useForm } from 'react-hook-form';
+// import axiosInstance from "@/axiosInstance";
+import { toast } from 'react-toastify';
+import { FaSpinner } from 'react-icons/fa';
+import { useContext, useEffect, useState } from 'react';
+import EventContext from '../../../context/Event/EventContext';
 
-// define your extension array
 const extensions = [StarterKit];
 
-const content = '<p>Hello World!</p>';
+// const fetchDataEndpoint = (atividade_id) => {
+//   return `/admin/eventos/${atividade_id}`;
+// };
 
-const Tiptap = () => {
-	const editor = useEditor({
-		extensions,
-		content,
-	});
-
-
-  const buttonStyle = 'p-2'
-	return (
-		<>
-			<EditorContent editor={editor} />
-			<FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-			<BubbleMenu editor={editor}>
-				<button className={buttonStyle} onClick={() => console.log(JSON.stringify(editor.getJSON()))}>
-					show OUtput
-				</button>
-				<button
-					onClick={() => editor.chain().focus().toggleBold().run()}
-					disabled={!editor.can().chain().focus().toggleBold().run()}
-					className={buttonStyle}
-				>
-					Bold
-				</button>
-				<button
-					onClick={() => editor.chain().focus().toggleItalic().run()}
-					disabled={!editor.can().chain().focus().toggleItalic().run()}
-					className={buttonStyle}
-				>
-					Italic
-				</button>
-				<button
-					onClick={() => editor.chain().focus().toggleStrike().run()}
-					disabled={!editor.can().chain().focus().toggleStrike().run()}
-					className={buttonStyle}
-				>
-					Strike
-				</button>
-			</BubbleMenu>
-		</>
-	);
-};
 
 const AdminEdicaoEvento = () => {
+	const { getEventDataById } = useContext(EventContext);
 	const { event_id } = useParams();
+	const [evento, setEvento] = useState({});
+	// const {isLoading, setIsLoading} = useState({});
+
+	const editor = useEditor({
+		extensions,
+		editorProps: {
+			attributes: {
+				class:
+					'prose max-w-full text-black bg-zinc-50 rounded-b-lg p-2 focus:outline-none w-full max-h-[400px] overflow-y-scroll',
+			},
+		}
+	});
+	const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+    setValue,
+  } = useForm({
+    mode: "all",
+  });
+
+	async function onSubmit(data) {
+    // try {
+    //   await axiosInstance.put(fetchDataEndpoint(event_id), {
+    //     ...data,
+    //   });
+    //   toast.success("Atividade Atualizado!");
+    // } catch (error) {
+    //   console.log(error);
+    //   toast.error("Erro ao atualizar Atividade!");
+    // }
+		toast.info('Ainda não temos podemos enviar nada pessoal, em fase experimental hehehe')
+
+		console.log(data)
+		console.log(evento)
+		console.log('JSON', editor.getJSON())
+		console.log('HTML', editor.getHTML())
+		console.log('TXT ', editor.getText())
+  }
+
+	useEffect(() => {
+    const fetchData = async () => {
+      const response = getEventDataById(event_id)
+
+      setEvento(response);
+
+      const { nome, data } =
+        response;
+
+      setValue("nome", nome);
+      setValue("data", data);
+  
+      // setIsLoading(false);
+    };
+
+    fetchData();
+  }, [setValue, event_id, setEvento, getEventDataById]);
+
 
 	return (
-		<>
-			<Tiptap />
+		<div className='w-full h-screen'>
+			
 			<h1>esse é o id do evento {event_id}</h1>
-		</>
+
+			<form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-10 mx-auto bg-white p-7 rounded-lg text-black shadow"
+      >
+        <div className="flex flex-col gap-4">
+          <h2 className="text-lg font-bold">Dados Publicos</h2>
+          <div className="grid gap-4 grid-cols-1">
+            <div className="flex flex-col">
+              <label
+                htmlFor="first_name"
+                className="block mb-2 text-sm text-gray-900 font-bold"
+              >
+                Nome
+              </label>
+              <input
+                required
+                type="text"
+                id="first_name"
+                placeholder="Nome"
+                className={`${
+                  isSubmitting ? "blurred" : ""
+                } input text-gray-900 bg-white shadow border border-gray-300 rounded h-10 p-3`}
+                {...register("nome", { required: true })}
+                disabled={isSubmitting}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label
+                htmlFor="descricao"
+                className="block mb-2 text-sm text-gray-900 font-bold"
+              >
+                Descricao
+              </label>
+              <TextEditor editor={editor}/>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <div className="flex items-center justify-center gap-4 flex-col sm:flex-row">
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="btn-primary inline-flex items-center justify-center rounded h-10 bg-green-400 w-60 text-white font-bold"
+          >
+            {isSubmitting ? (
+              <>
+                <FaSpinner className="animate-spin mr-2" />
+                Aguarde...
+              </>
+            ) : (
+              "Salvar"
+            )}
+          </button>
+        </div>
+      </form>
+		</div>
 	);
 };
 
