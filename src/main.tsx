22e4d63841953b9580a0a82b4d0e6d143909b981
samuelfsd <samuel.estudo778@gmail.com>
2024-12-13 17:54:38 -0300

    chore: adiciona eslint, prettier e plugin para o tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import { StrictMode } from 'react'
-import { App } from './App'
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { App } from './App';
 
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-)
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
--- src/paths.ts
+++ src/paths.ts
@@ -1,11 +1,11 @@
 export const paths = {
-    participante: "/participante",
-    loja: "/loja",
-    atividades: "/atividades",
-    credenciamento: "/credenciamento",
-    inscritos: "/inscritos",
-    home: "/",
-    lotes: "/lotes",
-    sorteio_inscritos: "/inscritos/sorteio",
-    eventos: "/eventos"
-}
+  participante: '/participante',
+  loja: '/loja',
+  atividades: '/atividades',
+  credenciamento: '/credenciamento',
+  inscritos: '/inscritos',
+  home: '/',
+  lotes: '/lotes',
+  sorteio_inscritos: '/inscritos/sorteio',
+  eventos: '/eventos',
+};
