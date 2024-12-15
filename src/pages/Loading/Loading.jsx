0ce6347ea74e3ddd6e279dcddac444b70fd8a6e6
samuelfsd <samuel.estudo778@gmail.com>
2024-12-15 12:24:28 -0300

    chore: remove arquivos sem uso

--- src/pages/Loading/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Loading.module.css';
-
-const Loading = () => (
-  <div className={styles.loadingSpinner}>
-    <div className={styles.spinner}></div>
-  </div>
-);
-
-export default Loading;
