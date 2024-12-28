3b6f5ead35ccd090b2b5bcadb7c511ff22d00343
samuelfsd <samuel.estudo778@gmail.com>
2024-12-28 14:26:16 -0300

    feat: muda visual do componente de paginação

--- src/core/components/Pagination/index.tsx
+++ src/core/components/Pagination/index.tsx
@@ -1,4 +1,5 @@
-import { CaretLeft, CaretRight } from '@phosphor-icons/react';
+import { Button } from '../ui/button';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 interface PaginationProps {
   usersPerPage: number;
@@ -34,36 +35,34 @@ export function Pagination({
   );
 
   return (
-    <div className='flex flex-col items-center justify-center py-3'>
-      <nav className='block'></nav>
-      <div className='flex items-center justify-center'>
-        <button
-          onClick={paginateBack}
-          className='relative mr-1 inline-flex items-center rounded-l-md border border-gray-300 bg-white p-2 text-sm font-medium text-gray-500 hover:bg-gray-50'
-        >
-          <CaretLeft size={20} />
-        </button>
-        <div className='flex items-center gap-1'>
-          {pages.map(page => (
-            <button
-              className={`relative inline-flex items-center rounded border px-3 py-2 ${
-                page === currentPage
-                  ? 'border-blue-600 bg-blue-600 text-white'
-                  : 'border-gray-300 bg-white'
-              } text-sm font-medium leading-tight text-gray-500 hover:opacity-70`}
-              onClick={() => paginateToggle(page)}
-              key={page}
-            >
-              {page}
-            </button>
-          ))}
+    <div className='flex items-center justify-between'>
+      <span className='text-sm text-muted-foreground'>
+        Total de {totalUsers} item(s)
+      </span>
+      <div className='flex items-center gap-6 lg:gap-8'>
+        <div className='flex text-sm font-medium'>
+          Página {currentPage} de {total_pages}
         </div>
-        <button
-          onClick={paginateFront}
-          className='relative ml-1 inline-flex items-center rounded-r-md border border-gray-300 bg-white p-2 text-sm font-medium text-gray-500 hover:bg-gray-50'
-        >
-          <CaretRight size={20} />
-        </button>
+        <div className='flex items-center gap-2'>
+          <Button
+            type='button'
+            onClick={paginateBack}
+            variant='outline'
+            className='h-8 w-8 p-0'
+          >
+            <ChevronLeft className='h-4 w-4' />
+            <span className='sr-only'>Primeira página</span>
+          </Button>
+          <Button
+            type='button'
+            onClick={paginateFront}
+            variant='outline'
+            className='h-8 w-8 p-0'
+          >
+            <ChevronRight className='h-4 w-4' />
+            <span className='sr-only'>Primeira página</span>
+          </Button>
+        </div>
       </div>
     </div>
   );
