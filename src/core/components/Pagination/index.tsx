e9e4dfeda61fd967e6dd74a06e6aa3f86f131727
samuelfsd <samuel.estudo778@gmail.com>
2024-12-29 15:01:48 -0300

    feat: aiciona novos botões na paginação

--- src/core/components/Pagination/index.tsx
+++ src/core/components/Pagination/index.tsx
@@ -1,5 +1,10 @@
 import { Button } from '../ui/button';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from 'lucide-react';
 
 interface PaginationProps {
   usersPerPage: number;
@@ -8,7 +13,7 @@ interface PaginationProps {
 
   paginateFront: () => void;
   paginateBack: () => void;
-  paginateToggle: (page: number) => void;
+  paginateToggle?: (page: number) => void;
 }
 
 export function Pagination({
@@ -39,10 +44,12 @@ export function Pagination({
       <span className='text-sm text-muted-foreground'>
         Total de {totalUsers} item(s)
       </span>
+
       <div className='flex items-center gap-6 lg:gap-8'>
         <div className='flex text-sm font-medium'>
           Página {currentPage} de {total_pages}
         </div>
+
         <div className='flex items-center gap-2'>
           <Button
             type='button'
@@ -50,9 +57,20 @@ export function Pagination({
             variant='outline'
             className='h-8 w-8 p-0'
           >
-            <ChevronLeft className='h-4 w-4' />
+            <ChevronsLeft className='h-4 w-4' />
             <span className='sr-only'>Primeira página</span>
           </Button>
+
+          <Button
+            type='button'
+            onClick={paginateBack}
+            variant='outline'
+            className='h-8 w-8 p-0'
+          >
+            <ChevronLeft className='h-4 w-4' />
+            <span className='sr-only'>Página anterior</span>
+          </Button>
+
           <Button
             type='button'
             onClick={paginateFront}
@@ -60,7 +78,17 @@ export function Pagination({
             className='h-8 w-8 p-0'
           >
             <ChevronRight className='h-4 w-4' />
-            <span className='sr-only'>Primeira página</span>
+            <span className='sr-only'>Próxima página</span>
+          </Button>
+
+          <Button
+            type='button'
+            onClick={paginateFront}
+            variant='outline'
+            className='h-8 w-8 p-0'
+          >
+            <ChevronsRight className='h-4 w-4' />
+            <span className='sr-only'>Última página</span>
           </Button>
         </div>
       </div>
