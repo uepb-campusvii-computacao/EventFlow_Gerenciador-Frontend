a087f262c4bd0e5a65024fb65d914646fb1815b5
samuelfsd <samuel.estudo778@gmail.com>
2025-01-21 23:54:38 -0300

    feat: adiciona componente de spinner

--- src/core/components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-
-export function Loading() {
-  return (
-    <div className={styles.loadingSpinner}>
-      <div className={styles.spinner}></div>
-    </div>
-  );
-}
